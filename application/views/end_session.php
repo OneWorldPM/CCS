80e2f9bff82b3c1569824930544e52b7bb949723
Rexter Dayuta <rexterdayuta@gmail.com>
2021-06-02 15:10:09 +0800

    temporary fix end session message

--- application/views/end_session.php
+++ application/views/end_session.php
@@ -1,4 +1,3 @@
-<?php //print_r(); exit;?>
 <style>
     .jumbotron{
         width: 900px;
@@ -38,10 +37,8 @@
 <section class="parallax fullscreen">
 <div class="container container-fullscreen">
     <div class="jumbotron">
-        <h2><?= (isset($sessions[0]->session_end_message) && !empty($sessions[0]->session_end_message))?$sessions[0]->session_end_message : 'This session is now closed.'?></h2>
+        <h2>This session is now closed.</h2>
     </div>
 </div>
 </section>
-<script>
 
-</script>
